91129267041dca5ee9e8fd6f1cff2747d1a0ffbe
nancymcclung <58371836+nancymcclung@users.noreply.github.com>
2019-11-30 14:18:27 -0700

    Update basic-math-3.js

--- src/basic-math-3.js
+++ src/basic-math-3.js
@@ -1,22 +1,34 @@
 const x = 1
 const y = 2
 const a = 3
-const b = 4
+const b = 5
 
 console.log('x=',x,'y=',y,'a=',a,'b=',b)
 
-let z = ((b+a)*y*x)
+let z = (x + y +a+b)
 
 console.log('z=',z)
 
-z = z * 3
+z = z * 5
 
 console.log('z=',z)
 
-if (z > 30) {
+if (z > 40) {
   console.log('wow, z is huge', z)
-} else if (z>15){
+} else if (z>25){
   console.log('nice z is just right', z)
 } else {
   console.log('little z', z)
+}
+
+z = z *1
+
+console.log('z=', z)
+
+if (z > 75) {
+    console.log('z is bigger than before', z)
+} else if (z>50){
+    console.log('z is in the middle', z)
+} else {
+    console.log('z is smaller than i hoped',z)
 }
